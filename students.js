b6c2bebf4f5c8770e4febcf2f32608329a12191a
mohammad <mohammadmajidi27@gmail.com>
2021-12-02 23:10:46 +0330

    c

--- students.js
+++ students.js
@@ -11,32 +11,34 @@ function addStudent(student) {
 
 
 function add_student() {
-    const newStudent = []
     let idpr = prompt("please enter your id")
     let namepr = prompt("please enter your name")
     let lastNamepr = prompt("please enter your lastName")
-    Students.push(idpr)
-    Students.push(namepr)
-    Students.push(lastNamepr)
+    students.push({id: idpr, name: namepr, lastName: lastNamepr})
     renderTable()
 
 
 }
 
 function removeStudent(id) {
-    students.filter((student) => student.id !== id)
+    students.filter((student) => student["id"] !== id)
     renderTable()
 }
 
 function updateStudent(id, newStudent) {
+    let updateIdpr = prompt("please enter your id")
+    let updateNamepr = prompt("please enter your name")
+    let updateLastNamepr = prompt("please enter your lastName")
+    newStudent = {id: updateIdpr, name: updateNamepr, lastName: updateLastNamepr}
     students.map((student) => student.id === id ? newStudent : student)
+
 }
 
 function renderTable() {
     const table = document.getElementById("mainTable")
     const tbody = document.getElementById("tableTbody")
-    tbody.innerHTML=null
+    tbody.innerHTML = null
     students.forEach((student) => {
-        tbody.innerHTML += (`<tr><td>${student["id"]}</td><td>${student["name"]} </td><td>${student["lastName"]}</td><td><button id="rmvStudent" onclick="removeStudent($student["id"])>Remove</button></td></tr>`)
+        tbody.innerHTML += (`<tr><td>${student["id"]}</td><td>${student["name"]} </td><td>${student["lastName"]}</td><td><button id="removeStudent" onclick="removeStudent($student["id"])>Remove</button><button id="editBtn" onclick="updateStudent($student["id"],$newStudent)>Edit</button></td></tr>`)
     })
 }
